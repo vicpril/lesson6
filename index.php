45cde46ec00cd263307495162550b1debdbee6f3
Viktor Prilepin <vicpride@gmail.com>
2015-03-19 23:29:15 +0600

    Work on bugs - 2

--- index.php
+++ index.php
@@ -6,9 +6,8 @@ session_start();
 //
 // Функции вывода списков городов, метро, категорий
 //
-function show_city_block($city = '') {
+function show_city_block($gorod = '') {
     $cities = array('641780' => 'Новосибирск', '641490' => 'Барабинск', '641510' => 'Бердск', '641600' => 'Искитим', '641630' => 'Колывань', '641680' => 'Краснообск', '641710' => 'Куйбышев', '641760' => 'Мошково', '641790' => 'Обь', '641800' => 'Ордынское', '641970' => 'Черепаново',);
-    $gorod = (isset($_GET['show'])) ? $_SESSION['explanation'][$_GET['show']]['location_id'] : null;
     ?>
 
     <select title="Выберите Ваш город" name="location_id"> 
@@ -24,9 +23,8 @@ function show_city_block($city = '') {
     <?php
 }
 
-function show_subway_block($subway = '') {
+function show_subway_block($metro = '') {
     $subways = array('2028' => 'Берёзовая роща', '2018' => 'Гагаринская', '2017' => 'Заельцовская', '2029' => 'Золотая Нива', '2019' => 'Красный проспект', '2027' => 'Маршала Покрышкина', '2021' => 'Октябрьская', '2025' => 'Площадь Гарина-Михайловского', '2020' => 'Площадь Ленина', '2024' => 'Площадь Маркса', '2022' => 'Речной вокзал', '2026' => 'Сибирская', '2023' => 'Студенческая',);
-    $metro = (isset($_GET['show'])) ? $_SESSION['explanation'][$_GET['show']]['metro_id'] : null;
     ?>
 
     <select title="Выберите станцию метро" name="metro_id"> 
@@ -41,16 +39,15 @@ function show_subway_block($subway = '') {
     <?php
 }
 
-function show_category_block($cat = '') {
+function show_category_block($category = '') {
     $categories = array('GR0' => 'Транспорт', '9' => 'Автомобили с пробегом', '109' => 'Новые автомобили', '14' => 'Мотоциклы и мототехника', '81' => 'Грузовики и спецтехника', '11' => 'Водный транспорт', '10' => 'Запчасти и аксессуары', 'GR1' => 'Недвижимость', '24' => 'Квартиры', '23' => 'Комнаты', '25' => 'Дома, дачи, коттеджи', '26' => 'Земельные участки', '85' => 'Гаражи и машиноместа', '42' => 'Коммерческая недвижимость', '86' => 'Недвижимость за рубежом', 'GR2' => 'Работа', '111' => 'Вакансии (поиск сотрудников)', '112' => 'Резюме (поиск работы)', 'GR3' => 'Услуги', '114' => 'Предложения услуг', '115' => 'Запросы на услуги', 'GR4' => 'Личные вещи', '27' => 'Одежда, обувь, аксессуары', '29' => 'Детская одежда и обувь', '30' => 'Товары для детей и игрушки', '28' => 'Часы и украшения', '88' => 'Красота и здоровье', 'GR5' => 'Для дома и дачи', '21' => 'Бытовая техника', '20' => 'Мебель и интерьер', '87' => 'Посуда и товары для кухни', '82' => 'Продукты питания', '19' => 'Ремонт и строительство', '106' => 'Растения', 'GR07' => 'Бытовая техника', '32' => 'Аудио и видео', '97' => 'Игры, приставки и программы', '31' => 'Настольные компьютеры', '98' => 'Ноутбуки', '99' => 'Оргтехника и расходники', '96' => 'Планшеты и электронные книги', '84' => 'Телефоны', '101' => 'Товары для компьютера', '105' => 'Фототехника', 'GR6' => 'Хобби и отдых', '33' => 'Билеты и путешествия', '34' => 'Велосипеды', '83' => 'Книги и журналы', '36' => 'Коллекционирование', '38' => 'Музыкальные инструменты', '102' => 'Охота и рыбалка', '39' => 'Спорт и отдых', '103' => 'Знакомства', 'GR7' => 'Животные', '89' => 'Собаки', '90' => 'Кошки', '91' => 'Птицы', '92' => 'Аквариум', '93' => 'Другие животные', '94' => 'Товары для животных', 'GR8' => 'Для бизнеса', '116' => 'Готовый бизнес', '40' => 'Оборудование для бизнеса',);
-    $id = (isset($_GET['show'])) ? $_SESSION['explanation'][$_GET['show']]['category_id'] : null;
     ?>
 
     <select title="Выберите категорию объявления" name="category_id"> 
         <option value="">-- Выберите категорию --</option>
         <?php
         foreach ($categories as $number => $cat) {
-            $selected = ($number == $id) ? 'selected=""' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
+            $selected = ($number == $category) ? 'selected=""' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
             echo '<optgroup>';
             if (!is_numeric($number)) {
                 echo '</optgroup><optgroup label="' . $cat . '">';
@@ -65,21 +62,32 @@ function show_category_block($cat = '') {
 }
 
 //
-// Функции удвления и вывода объявлений
+// Функции добавления удвления и вывода объявлений
 //
+function addExplanation($id) {
+    if ($id == '') {
+        $_SESSION['explanation'][] = $_POST;
+    } else {
+        $_SESSION['explanation'][$id] = $_POST;
+    }
+}
+
 function deleteExplanation($name) {
     unset($_SESSION['explanation'][$name]);
 }
 
-function showExplanation($Exp, $changing = false) {
-    $name = $_SESSION['explanation'][$Exp];
+function showExplanation($id, $changing = false) {
+    $name = (isset($_SESSION['explanation'][$id])) ? $_SESSION['explanation'][$id] :
+            array('private' => '1', 'seller_name' => '', 'email' => '', 'phone' => '',
+        'location_id' => '', 'metro_id' => '', 'category_id' => '', 'title' => '', 'description' => '',
+        'price' => '0');
     ?>
     <!DOCTYPE HTML>
     <html>
         <body>
-            <form  method="post" action="index.php?delete=<?= $Exp ?>">
+            <form  method="post" action="index.php?id=<?= $id ?>">
                 <?php
-                if ($name['private']) {
+                if ($name['private'] == '1') {
                     echo '<div>
                             <label>
                             <input type="radio" checked="" value="1" name="private">Частное лицо
@@ -121,14 +129,10 @@ function showExplanation($Exp, $changing = false) {
                 <div>
                     <label>Город</label>
                     <?php show_city_block($name['location_id']); ?>   
-                    <div> 
-                        <label>Станция метро</label>
-                        <?php show_subway_block($name['metro_id']);  //блок выбора метро  ?>
-                    </div>
                 </div>
                 <div>
                     <label>Категория</label> 
-                    <?php show_category_block($name['category_id']);           //блок выбора категории  ?>
+                    <?php show_category_block($name['category_id']);    //блок выбора категории   ?>
                 </div>
                 <div>
                     <label>Название объявления</label>
@@ -143,115 +147,63 @@ function showExplanation($Exp, $changing = false) {
                     <input type="text" maxlength="9" value="<?= $name['price'] ?>" name="price">&nbsp; <span>руб.</span>
                 </div>
                 <div>
-                    <input type="submit" value="Изменить объявление">
+                    <input type="submit" value="<?php
+                                                    if ($id == '') {
+                                                        echo 'Подать объявление';
+                                                    } else {
+                                                        echo 'Изменить объявление';
+                                                    }
+                                                ?>">
                 </div>
             </form>
-            <p><a href="index.php">Вернуться на предыдущую страницу</a></p>
-        </body>
-
-        <?php
-        exit;
-    }
-
-//
-// Главный блок: вывод формы, вывод объявлений, обработка запросов
-//
-    if (isset($_GET['show'])) {
-        showExplanation($_GET['show']);
-    }
-    if (isset($_GET['delete'])) {
-        deleteExplanation($_GET['delete']);
-    }
-    ?>
-
-    <!DOCTYPE HTML>
-    <html>
-        <body>
-            <form  method="post">
-                <div>
-                    <label>
-                        <input type="radio" checked="" value="1" name="private">Частное лицо</label>
-                    <label>
-                        <input type="radio" value="0" name="private">Компания
-                    </label>
-                </div>
-                <div>
-                    <label><b>Ваше имя</b></label>
-                    <input type="text" maxlength="40" value="" name="seller_name">
-                </div>
-                <div>
-                    <label>Электронная почта</label>
-                    <input type="text" value="" name="email">
-                </div>
-                <div>
-                    <label>
-                        <input type="checkbox" value="1" name="allow_mails">
-                        <span>Я не хочу получать вопросы по объявлению по e-mail</span>
-                    </label> 
-                </div>
-                <div>
-                    <label>Номер телефона</label>
-                    <input type="text" value="" name="phone">
-                </div>
-                <div>
-                    <label>Город</label>
-                    <?php show_city_block();    //блок выбора города   ?>   
-
-                    <div> 
-                        <label>Станция метро</label>
-                        <?php show_subway_block();  //блок выбора метро   ?>
-                    </div>
-                </div>
-                <div>
-                    <label>Категория</label> 
-                    <?php show_category_block();           //блок выбора категории   ?>
-                </div>
-                <div>
-                    <label>Название объявления</label>
-                    <input type="text" maxlength="50" value="" name="title"> 
-                </div>
-                <div>
-                    <label>Описание объявления</label> 
-                    <textarea maxlength="3000" name="description"></textarea> 
-                </div>
-                <div> 
-                    <label>Цена</label> 
-                    <input type="text" maxlength="9" value="0" name="price">&nbsp; <span>руб.</span>
-                </div>
-                <div>
-                    <input type="submit" value="Подать объявление">
-                </div>
-            </form>
-
-
             <?php
-            if ($_POST) {
-                $_SESSION['explanation'][] = $_POST;
+            if ($id !== '') {
+                echo '<form method="get">
+                        <div>
+                        <button value="index.php">Отмена</button>
+                        </div>
+                    </form>';
             }
-//            print_r($_SESSION);
-            if ($_SESSION) {
-                echo "<h2>Объявления</h2>";
+        }
 
-                echo "<table width = 100%>";
-                echo "<tr>";
-                echo "<th bgcolor='silver'>Название объявления</th>";
-                echo "<th bgcolor='silver'>Цена</th>";
-                echo "<th bgcolor='silver'>Имя</th>";
-                echo "<th bgcolor='silver'>Удалить</th>";
+//
+// Главный блок: обработка запросов, вывод формы, вывод объявлений
+//
+        $id = (isset($_GET['id'])) ? $_GET['id'] : '';
+
+        if (isset($_GET['delete'])) {
+            deleteExplanation($_GET['delete']);
+        }
+
+        if ($_POST) {
+            addExplanation($id);
+        }
+        showExplanation($id);
+        //print_r($_SESSION);
+
+
+        echo "<h2>Объявления</h2>";
+
+        echo "<table width = 100%>";
+        echo "<tr>";
+        echo "<th bgcolor='silver'>Название объявления</th>";
+        echo "<th bgcolor='silver'>Цена</th>";
+        echo "<th bgcolor='silver'>Имя</th>";
+        echo "<th bgcolor='silver'>Удалить</th>";
+        echo "</tr>";
+        if ($_SESSION) {
+            foreach ($_SESSION['explanation'] as $key => &$value) {
+                echo "<tr align = 'center'>";
+                echo "<td><a href='index.php?id=" . $key . "'>" . $value['title'] . "</td>";
+                echo "<td>" . $value['price'] . "</td>";
+                echo "<td>" . $value['seller_name'] . "</td>";
+                echo "<td><a href='index.php?delete=" . $key . "'>Удалить</a></td>";
                 echo "</tr>";
-
-                foreach ($_SESSION['explanation'] as $key => &$value) {
-                    echo "<tr align = 'center'>";
-                    echo "<td><a href='index.php?show=" . $key . "'>" . $value['title'] . "</td>";
-                    echo "<td>" . $value['price'] . "</td>";
-                    echo "<td>" . $value['seller_name'] . "</td>";
-                    echo "<td><a href='index.php?delete=" . $key . "'>Удалить</a></td>";
-                    echo "</tr>";
-                }
-                echo "</table>";
             }
+            echo "</table>";
+        }
 //session_destroy();
-            ?>
-        </body>
-    </html>
+        ?>
+    </body>
+</html>
 
